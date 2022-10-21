cf4a8dfa257f805eb75dc7ed470b449d300a1c2c
Andhika26120ID <116289054+Andhika26120ID@users.noreply.github.com>
2022-10-21 16:19:50 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,32 @@
+// NAMA		: Muhammad Daffa Andhika Putra
+// NRP		: 5022211191
+// Jurusan	: Teknik Elektro
+
+#include <iostream>
+#include <cmath>
+
+#define GRAVITASI 10 //10 m/s^2
+#define START_PENGUKURAN 1 //pengukuran dimulai dari 1 meter
+#define SUDUT 45 //sudut elevasi tembakan
+
+tipe_data mencari_V0(tipe_data variabel1, tipe_data variabel2)
+{
+	/* int (mencari_V0 = 30 ) */
+  	return 
+}
+
+tipe_data speed_dgn_loss(tipe_data variabel)
+{
+	/* tulis fungsi hitung_loss kalian disini */
+    return
+}
+
+int main() {
+    /* tulis kode utama kalian disini */
+  
+  	/* input adalah kecepatan tangensial maksimum roller */
+  	/* std::cin >> input */
+  
+  	/* std::cout << jarak << " " << kecepatan tangensial << std::endl */
+    return 0;
+}
